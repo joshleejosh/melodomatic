d44594eaaf997a982ac5c3e71b7ee637e0d208fe
Josh Lee <josh@floor.is>
2017-05-07 11:07:06 -0700

    test coverage exclusions

--- test/run_tests_coverage.py
+++ test/run_tests_coverage.py
@@ -8,7 +8,15 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'melodomatic'))
 import consts
 
 def do_tests():
-    coverer = coverage.Coverage(source=('melodomatic',))
+    coverer = coverage.Coverage(
+            source=('melodomatic',),
+            omit=('melodomatic/__main__.py',
+                'melodomatic/consts.py',
+                'melodomatic/midi.py',
+                'melodomatic/viz.py',
+                'melodomatic/vizcurses/__init__.py'))
+    coverer.exclude('consts\.VERBOSE:')
+    coverer.exclude('def dump\(self\):')
     coverer.start()
 
     # -----------------------------------------------
